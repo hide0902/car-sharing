93ecbf93ce3b8f581ac672648501194fda4f5302
hide0902 <hn.tt.sf.hu@icloud.com>
2020-03-21 00:53:08 +0900

    ルーティング、コントローラー設定

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,13 @@
 class TopsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Car.new
+    @item.images.new
+  end
+
+  def create
+    @item = Car.new(car_params)
+  end
 end
