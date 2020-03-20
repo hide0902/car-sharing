0e054819eb7a9651dd2f4ee4c5bceb74d79c3d53
hide0902 <hn.tt.sf.hu@icloud.com>
2020-03-20 17:42:23 +0900

    carモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_105353) do
+ActiveRecord::Schema.define(version: 2020_03_20_082114) do
+
+  create_table "cars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "maker"
+    t.integer "price"
+    t.integer "period"
+    t.integer "gas"
+    t.integer "type"
+    t.integer "handle"
+    t.integer "capa"
+    t.string "prefecture"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -26,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_03_19_105353) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cars", "users"
 end
