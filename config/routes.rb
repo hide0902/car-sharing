f027fd9570671e11d37d2e8640a5b30f6ba86b2e
hide0902 <hn.tt.sf.hu@icloud.com>
2020-04-02 17:08:44 +0900

    編集機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
-  resources :cars, only: [ :index, :new, :create, :show ] do
+  resources :cars, only: [ :index, :new, :create, :show, :edit, :update ] do
     resources :requests, only: [ :create ]
     resources :likes, only: [:create, :destroy]
     resources :reviews, only: [ :create ]
