e7b8cc3e717d0a888988be62391e04dd30c9ebe4
hide0902 <hn.tt.sf.hu@icloud.com>
2020-04-06 23:32:29 +0900

    レビュー非同期1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  $('.car-show__box__review-box__write__form').on('submit', function(e){
+    console.log('hoge');
+    e.preventDefault();
+    // var formData = new FormData(this);
+    // var url = $(this).attr('action');
+    // $.ajax({
+    //   url: url,
+    //   type: "POST",
+    //   data: formData,
+    //   dataType: 'json',
+    //   processData: false,
+    //   contentType: false
+    // })
+  });
+
+});
